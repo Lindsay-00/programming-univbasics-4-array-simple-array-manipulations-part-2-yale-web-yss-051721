cada3ae224969eeb216e16cd3e0b6c23c73abc75
Lindsay Chen <lindsay.chen@yale.edu>
2021-06-02 18:17:28 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+def using_concat (array1, array2)
+  array1.concat(array2)
+end
+my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+more_favs = ["sports cars", "flatiron school"]
+p using_concat my_favorite_things, more_favs
+
+def using_insert (array, element)
+  array.insert(3, element)
+end
+list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+another_language = "Python"
+p using_insert list_of_programming_languages another_language
